957bfcdc6347bb69a52153aab31a0bf816bd6cfe
Surjit Sahoo <surjitsahoo0@gmail.com>
2021-04-23 21:37:18 +0530

    trying to add insta posts

--- index.js
+++ index.js
@@ -7,12 +7,15 @@ const TEMPLATE_FILE = './README.template.md';
 let DATA = {};
 
 async function setInstagramPosts() {
-  const instagramImages = await puppeteerService.getLatestInstagramPostsFromAccount('nanhaajaan', 5);
+  const instagramImages = await puppeteerService.getLatestInstagramPostsFromAccount('nanhaajaan', 8);
   DATA.img1 = instagramImages[0];
   DATA.img2 = instagramImages[1];
   DATA.img3 = instagramImages[2];
   DATA.img4 = instagramImages[3];
   DATA.img5 = instagramImages[4];
+  DATA.img6 = instagramImages[5];
+  DATA.img7 = instagramImages[6];
+  DATA.img8 = instagramImages[7];
 }
 
 async function generateReadMe() {
